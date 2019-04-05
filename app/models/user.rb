495956198a1ed346b926c2b7e73797b8329c8d57
James Luke Johnson <jjohn144@my.westga.edu>
2019-04-05 08:46:02 -0400

    Added username to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, authentication_keys: [:login]
+
+ # Virtual attribute for authenticating by either username or email
+ # This is in addition to a real persisted field like 'username'
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication warden_conditions
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  end
+
 end

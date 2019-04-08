6a54942ef03f643df505dfaa8187208fd880573f
James Luke Johnson <jjohn144@my.westga.edu>
2019-04-08 12:31:24 -0400

    Edited restaurant controller tests to sign in first

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
+
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    @user = users(:one)
     @restaurant = restaurants(:unique)
   end
 
@@ -18,12 +22,18 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create restaurant" do
+    sign_in @user
     @restaurant.name = "Something different"
     assert_difference('Restaurant.count') do
       post restaurants_url, params: { restaurant: { city: @restaurant.city, cuisine: @restaurant.cuisine, name: @restaurant.name, postcode: @restaurant.postcode, state: @restaurant.state, street_address: @restaurant.street_address} }
     end
 
     assert_redirected_to restaurant_url(Restaurant.last)
+  end
+
+  test "should not allow creating if not signed in" do
+    post restaurants_url, params: { restaurant: { city: @restaurant.city, cuisine: @restaurant.cuisine, name: @restaurant.name, postcode: @restaurant.postcode, state: @restaurant.state, street_address: @restaurant.street_address} }
+    assert_redirected_to new_user_session_path
   end
 
   test "should show restaurant" do
@@ -35,13 +45,20 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get edit" do
+    sign_in @user
     get edit_restaurant_url(@restaurant)
     assert_response :success
   end
 
   test "should update restaurant" do
+    sign_in @user
     patch restaurant_url(@restaurant), params: { restaurant: { city: @restaurant.city, cuisine: @restaurant.cuisine, name: @restaurant.name, postcode: @restaurant.postcode, state: @restaurant.state, street_address: @restaurant.street_address, will_split: @restaurant.will_split, wont_split: @restaurant.wont_split } }
     assert_redirected_to restaurant_url(@restaurant)
+  end
+
+  test "should not allow editing if not signed in" do
+    patch restaurant_url(@restaurant), params: { restaurant: { city: @restaurant.city, cuisine: @restaurant.cuisine, name: @restaurant.name, postcode: @restaurant.postcode, state: @restaurant.state, street_address: @restaurant.street_address, will_split: @restaurant.will_split, wont_split: @restaurant.wont_split } }
+    assert_redirected_to new_user_session_path
   end
 
   test "can't destroy restaurant" do
@@ -60,11 +77,17 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should vote and add to votes array" do
+    sign_in @user
     get restaurant_url(@restaurant)
     assert_equal 0, session[:votes].count
     assert_equal 0, @restaurant.will_split
     put vote_restaurant_path(@restaurant), params: { split: "will_split" }
     assert_equal 1, session[:votes].count
+  end
+
+  test "should not allow voting if not signed in" do
+    put vote_restaurant_path(@restaurant), params: { split: "will_split" }
+    assert_redirected_to new_user_session_path
   end
 
   test "should clear the search term and page" do

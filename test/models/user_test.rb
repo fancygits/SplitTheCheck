3d5d6c59da86eee5667e634e9fd5743398d66da4
James Luke Johnson <jjohn144@my.westga.edu>
2019-04-08 12:33:20 -0400

    Edited restaurant controller tests to sign in first

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "user attributes must not be empty" do
+    u = User.new
+    assert u.invalid?
+    assert u.errors[:username].any?
+    assert u.errors[:email].any?
+    assert u.errors[:password].any?
+  end
+
 end

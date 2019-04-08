fd9d8d340ab5d1f65474647e814c1c1df4025b61
James Luke Johnson <jjohn144@my.westga.edu>
2019-04-08 11:56:40 -0400

    Modified user registration, edit, and login pages. Mostly stylistically.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
+         :recoverable, :rememberable, 
          :validatable, authentication_keys: [:login]
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
 
  # Virtual attribute for authenticating by either username or email
  # This is in addition to a real persisted field like 'username'
@@ -13,10 +16,20 @@ class User < ApplicationRecord
     @login || self.username || self.email
   end
 
-  def self.find_for_database_authentication warden_conditions
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
-    login = conditions.delete(:login)
-    where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_h).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
   end
 
 end

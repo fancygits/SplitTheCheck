feb9e87366667fac3ca67f36cd7cb3f774f3d81c
James Luke Johnson <jjohn144@my.westga.edu>
2019-04-24 09:12:49 -0400

    Generated Favorite model and added to User.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_231708) do
+ActiveRecord::Schema.define(version: 2019_04_24_130747) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_favorites_on_restaurant_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"

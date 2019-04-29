bb802c33183239876992a1cf93a6b8230fb7e5ee
= <jjohn144@my.westga.edu>
2019-04-28 21:21:35 -0500

    System tested the app successfully! Sprint 3 Final Commit!

--- test/system/restaurants_test.rb
+++ test/system/restaurants_test.rb
@@ -8,6 +8,8 @@ class RestaurantsTest < ApplicationSystemTestCase
   setup do
     @restaurant = restaurants(:magic)
     @user = users(:validUser)
+    @comment1 = comments(:one)
+    @comment2 = comments(:two)
   end
 
   test "visiting the index" do
@@ -71,10 +73,79 @@ class RestaurantsTest < ApplicationSystemTestCase
     Warden.test_reset!
   end
 
+  test "favoriting (and unfavoriting) a Restaurant" do
+    login_as @user
+    visit restaurants_url
+    fill_in "Restaurant name or city", with: @restaurant.name
+    click_on "Search"
+    assert_selector "tr", text: "Abracadabra", count: 1
+    assert_selector("svg.icon.star-empty")
+    find(".favorite").click
+    assert_selector("svg.icon.star-full")
+    find(".favorite").click
+    assert_selector("svg.icon.star-empty")
+    Warden.test_reset!
+  end
+
   test "search for a restaurant" do
     visit restaurants_url
     fill_in "Restaurant name or city", with: "unique"
     click_on "Search"
     assert_text "Unique New York"
+  end
+
+  test "creating a comment" do
+    login_as @user
+    visit restaurants_url
+    click_on "Abracadabra"
+
+    fill_in "comment_body", with: @comment2.body
+    click_on "Post"
+
+    assert_text "Comment was successfully created"
+    assert_selector "div.card-text", text: "This place is magical!"
+    Warden.test_reset!
+  end
+
+  test "deleting a comment" do
+    login_as @user
+    visit restaurants_url
+    click_on "Abracadabra"
+    fill_in "comment_body", with: @comment1.body
+    click_on "Post"
+    assert_text "Comment was successfully created"
+    assert_selector "div.card-text", text: "This is a comment."
+    page.accept_confirm do
+      click_on "Delete", match: :first
+    end
+    assert_text "Comment was successfully deleted."
+    Warden.test_reset!
+  end
+
+  test "viewing a user profile" do
+    login_as @user
+    visit restaurants_url
+    click_on "Abracadabra"
+    fill_in "comment_body", with: @comment2.body
+    click_on "Post"
+    assert_text "Comment was successfully created"
+    assert_selector("svg.icon.star-empty")
+    find(".favorite").click
+    assert_selector("svg.icon.star-full")
+
+    visit restaurants_url
+    click_on "MyString"
+    assert_selector "a", text: "0\nWill Split"
+    click_on "Will Split"
+    assert_selector "a", class: "disabled", text: "1\nWill Split"
+
+    visit profile_url
+    find("section.votes").click
+    assert_text "MyString"
+    find("section.comments").click
+    assert_text "This place is magical!"
+    find("section.favorites").click
+    assert_text "Abracadabra"
+    Warden.test_reset!
   end
 end
